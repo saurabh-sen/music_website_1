2c82a47a721bbcb94e17f7b5e34a7c3143de5cd0
Saurabh <saurabh.sen.cs19@ggits.net>
2020-09-26 02:05:58 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const port = 8000;
+// mongoose.connect('mongodb://localhost/ContactMusic', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/ContactMusic', { useNewUrlParser: true, useUnifiedTopology: true });
+// Define Mongoose Schema
+var contactSchema = new mongoose.Schema({
+    name: String,
+    phone: Number,
+    email: String,
+    url: String,
+    desc: String
+});
+
+var Contact = mongoose.model('Contact', contactSchema);
+
+// EXPRESS RELATED STUFF
+app.use("/static", express.static("static")); // For serving static files
+app.use(express.urlencoded({ extended: true })); // Fetches web data
+
+// PUG RELATED STUFF
+app.set("view engine", "pug"); // Set the engine as pug
+app.set("views", path.join(__dirname, "views")); //Set the views directory
+
+// ENDPOINTS
+app.get("/", (req, res) => {
+    const params = { "title": "TMCofficialMusic" };
+    res.status(200).render("home.pug", params);
+});
+
+app.get("/contact", (req, res) => {
+    const params = { "title": "TMCofficialMusic" };
+    res.status(200).render("contact.pug", params);
+});
+
+app.post("/contact", (req, res) => {
+
+    var myData = new Contact(req.body);
+    myData.save().then(() => {
+        res.send("This item has been saved to the database")
+    }).catch(() => {
+        res.status(400).send("Item was not saved to the database")
+    });
+    // res.status(200).render("contact.pug", params);
+});
+
+// STARTS THE SERVER
+app.listen(port, () => {
+    console.log(`The server has started successfully at port ${port}`);
+});
