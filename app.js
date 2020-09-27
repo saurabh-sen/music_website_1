58ec5ed819261ed855d5912afe3becceb058fa31
Saurabh <saurabh.sen.cs19@ggits.net>
2020-09-26 23:04:13 -0700

    initial commit

--- app.js
+++ app.js
@@ -28,12 +28,12 @@ app.set("views", path.join(__dirname, "views")); //Set the views directory
 
 // ENDPOINTS
 app.get("/", (req, res) => {
-    const params = { "title": "TMCofficialMusic" };
+    const params = { "title": "tmcofficialmusic" };
     res.status(200).render("home.pug", params);
 });
 
 app.get("/contact", (req, res) => {
-    const params = { "title": "TMCofficialMusic" };
+    const params = { "title": "tmcofficialmusic" };
     res.status(200).render("contact.pug", params);
 });
 
